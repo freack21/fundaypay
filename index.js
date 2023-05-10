58936dfca48a97cf22c4eca781d29a395a338d77
freack21 <112834512+freack21@users.noreply.github.com>
2023-05-10 13:01:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post(`/`, async (req, res) => {
   const url = 'http://23.95.48.230:3020/donate'
   const data = req.body;
   const donate = await axios.post(url, data);
-  res.send(donate);
+  res.send("success");
 });
 
 app.listen(port, function() {

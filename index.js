e429701ab9c57f828ff51eca2cf8c7d5c489731d
freack21 <112834512+freack21@users.noreply.github.com>
2023-05-13 22:01:22 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,29 +2,27 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 8021;
+const port = process.env.PORT || 8020;
 
 app.use(cors());
 app.use(express.json());
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 
 app.get(`/`, (req, res) => {
-  // console.log(req)
-  res.send("running");
+    res.send("running");
 });
 
 app.post(`/`, async (req, res) => {
-  console.log(req)
-  const url = 'http://23.95.48.230:3020/donate'
-  const data = req.body;
-  const donate = await axios.post(url, data);
-  res.send("success");
+    const url = "http://23.95.48.230:3020/donate";
+    const data = req.body;
+    const donate = await axios.post(url, data);
+    res.status(200).send("success");
 });
 
-app.listen(port, function() {
-  console.log(`App running on http://localhost:` + port);
+app.listen(port, function () {
+    console.log(`App running on http://localhost:` + port);
 });

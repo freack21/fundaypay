05afd32f6e73873856affd95d915942415f67a55
freack21 <112834512+freack21@users.noreply.github.com>
2023-07-14 14:45:38 +0700

    Updating index.js

--- index.js
+++ index.js
@@ -26,8 +26,16 @@ app.post(`/`, async (req, res) => {
 app.post(`/send`, async (req, res) => {
     const url = "http://23.95.48.230:3020/send";
     const data = req.body;
-    res.status(200).send("success");
     const sender = await axios.post(url, data);
+    res.send(sender);
+});
+
+app.get(`/send`, async (req, res) => {
+    const url = "http://23.95.48.230:3020/send";
+    // const { msg, to, img, ext } = req.query;
+    const data = req.query;
+    const sender = await axios.post(url, data);
+    res.send(sender);
 });
 
 app.listen(port, function () {

93017ee1ca8d4507c7a3e1d2db66180d851f1795
Fikri Rivandi <112834512+freack21@users.noreply.github.com>
2023-09-24 00:20:52 +0700

    feat[downscrap]: add

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.get(`/`, (req, res) => {
     res.send("running");
 });
 
+app.all(`/downscrap/:type`, async (req, res) => {
+    const { type } = req.params;
+    const { url } = req.query;
+    const urls = `http://23.95.48.230:2121/${type}?url=${url}`;
+    const response = await axios.get(urls);
+    res.status(200).json(response.data);
+});
+
 app.post(`/`, async (req, res) => {
     const url = "http://23.95.48.230:3020/donate";
     const data = req.body;
